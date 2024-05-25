6121227b1242bc8f9c4860ccee61321810d130bb
DharshanMP <shand3649@gmail.com>
2024-05-25 09:54:05 +0530

    update : updated exception handling

--- Day26/NATO/main.py
+++ Day26/NATO/main.py
@@ -3,6 +3,14 @@ alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 file1 =  {row.letter:row.code for (index, row) in alphabet_data.iterrows()}
 
-user_input = input("enter the word: ").upper()
-li = [file1 [letter]for letter in user_input]
-print(li)
+def generate_phenetic():
+    user_input = input("enter the word: ").upper()
+    try:
+        li = [file1 [letter]for letter in user_input]
+    except KeyError:
+        print("sorry Alphabet's only allowed")
+        generate_phenetic()
+    else:    
+        print(li)
+
+generate_phenetic()
